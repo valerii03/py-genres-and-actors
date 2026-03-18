58c9183dc0d4631cb5628460d9953018dc6b969e
Valerii <valeraivasik602@gmail.com>
2026-03-18 21:40:55 +0000

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import init_django_orm  # noqa: F401
-
 from django.db.models import QuerySet
+from db.models import Genre, Actor
 
 
-def main() -> QuerySet:
-    pass
+def main() -> QuerySet[Actor]:
+    # Створюємо жанри
+    for name in ["Western", "Action", "Dramma"]:
+        Genre.objects.get_or_create(name=name)
+
+    # Створюємо акторів
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.get_or_create(first_name=first_name, last_name=last_name)
+
+    # Оновлення
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+
+    # Видалення
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # Повертаємо результат для тесту
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
