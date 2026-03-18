4a72b6bc113c38de954f6f1fa782be053fbee533
Valerii <valeraivasik602@gmail.com>
2026-03-18 21:42:41 +0000

    Solution

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet[Actor]:
-    # Створюємо жанри
     for name in ["Western", "Action", "Dramma"]:
         Genre.objects.get_or_create(name=name)
 
-    # Створюємо акторів
     actors = [
         ("George", "Klooney"),
         ("Kianu", "Reaves"),
@@ -17,16 +15,21 @@ def main() -> QuerySet[Actor]:
         ("Scarlett", "Johansson"),
     ]
     for first_name, last_name in actors:
-        Actor.objects.get_or_create(first_name=first_name, last_name=last_name)
+        Actor.objects.get_or_create(
+            first_name=first_name,
+            last_name=last_name
+        )
 
-    # Оновлення
     Genre.objects.filter(name="Dramma").update(name="Drama")
-    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
-    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(
+        last_name="Clooney"
+    )
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
 
-    # Видалення
     Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
-    # Повертаємо результат для тесту
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
